456ce76df70a0d7dfdd9d0e1e0fb32ad6c73a28f
DanKe123abc <1586017466@qq.com>
2025-05-03 18:28:08 +0800

    feat(app): 添加 Microsoft Clarity

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import { SidebarWrapper } from "@/components/sidebar-wrapper"
 import { SidebarButton } from "@/components/sidebar-button"
+import Clarity from '@microsoft/clarity';
 
 interface ClientLayoutProps {
   children: React.ReactNode
@@ -10,7 +11,11 @@ interface ClientLayoutProps {
 
 export default function ClientLayout({ children }: ClientLayoutProps) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
+  const projectId = process.env.CLARITY_PROJECT_ID;
+  if (!process.env.CLARITY_PROJECT_ID) {
+    throw new Error('CLARITY_PROJECT_ID is not defined');
+  }
+  Clarity.init(projectId!);
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
