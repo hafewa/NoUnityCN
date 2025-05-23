52d00a69c092e4ceb0bf75ab318c9ddf4c861803
Creation·Wong <chuangyuwang@creationwong.onmicrosoft.com>
2025-05-23 15:14:10 +0800

    添加下载页面 查看发行说明 按钮

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Page() {
             href={`./releaseNotes?v=${url}`}
         >
           <Share className="w-5 h-5 mr-2"/>
-          查看版本说明
+          查看发行说明
         </Button>
         <Button
             className="flex-initial"
